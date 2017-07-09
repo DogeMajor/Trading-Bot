b183529e9bca3e96fa77563a36dcdf1edef94164
DogeMajor <you@example.com>
2017-07-09 16:30:15 +0000

    "Testing istallations"

--- bin/ui.py
+++ bin/ui.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 
+
 import numpy as np
 import scipy
 from selenium import webdriver
-from selenium.webdriver.commonkeys import Keys
-url = 'www.wallstreetsurvivor.com'
+from selenium.webdriver.common.keys import Keys
+from credentials import user_name, password
+wall_str_url = 'https://www.wallstreetsurvivor.com/login'
+import tensorflow as tf
+print('test0!')
+
 
 class UI(object):
-
-    def __init__():
+    def __init__(self):
+        print('test!')
         self.driver = webdriver.Firefox()
 
-    def navigate_to(url):
+    def login(self, url):
         self.driver.get(url)
+        elem = self.driver.find_element_by_id('LoginModel_User_Name').click()
 
-if __name__ == 'main':
-    ui = UI()
-    ui.navigate_to(url)
+
+
+
+
+#if __name__=='main':
+
+#print(tf.__dict__)
+ui = UI()
+ui.login(wall_str_url)
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
